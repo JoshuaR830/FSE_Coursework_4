4f97c3305cda175214dc376ee8ff5388b47fa5a9
Julian Kyotaro Kubelec <juliankubelec23@gmail.com>
2018-04-27 09:30:03 +0100

    Made a test for the constructor: CompanyEmail

--- FSE Coursework 4/src/tests/TestCompanyEmail.java
+++ FSE Coursework 4/src/tests/TestCompanyEmail.java
@@ -6,18 +6,14 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class TestCompanyEmail {
+import emailScripts.CompanyEmail;
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+public class TestCompanyEmail {
 	
 //	Test default constructor
 	
-//	Test ID: L.N.N
+//	Test ID: A.1.1
 // 	Test created by: f_name s_name
-//	Date created: dd/mm/yy
 	
 //	Test ID: L.N.N
 // 	Test created by: f_name s_name
@@ -27,16 +23,17 @@ public class TestCompanyEmail {
 	
 //	Test main constructor
 	
-//	Test ID: L.N.N
-// 	Test created by: f_name s_name
-//	Date created: dd/mm/yy
-	
-//	Test ID: L.N.N
-// 	Test created by: f_name s_name
-//	Date created: dd/mm/yy
-	
-	
-	
+//	Test ID: A.2.1
+// 	Test created by: Julian Kubelec
+//	Date created: 26/04/18 
+@Test
+	public void testA21() {
+		String fAddress ="julian@hi.com";
+		String tAddress ="julian@hi.com";
+		String subLine = "ur mum gaye";
+		String eMessage = "no u";
+		assertNotNull(new CompanyEmail(fAddress, tAddress, subLine, eMessage));
+}
 //	Test get method for sender’s address
 	
 //	Test ID: L.N.N
