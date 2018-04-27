3be274a0a63bd962110ab93635f44c91cd796534
Tim <timbartrum73@gmail.com>
2018-04-27 15:03:16 +0100

    Adding refactored CompanyProject- cleaned up constructor. Still more to do

---
+++ FSE Coursework 4/src/emailScripts/CompanyProject2.java
@@ -0,0 +1,98 @@
+// https://docs.google.com/document/d/1migYYyWoCX3aydqz9rkFh2q5YYehMueO_p8LHfVqLb4/edit
+
+package emailScripts;
+
+import java.util.ArrayList;
+
+public class CompanyProject2 {
+    private int PID;
+    private String PTitle;
+    private ArrayList<String> ProjectContacts;
+    private int ProjectPhase;
+    private ArrayList[] ProjectEmails = new ArrayList[6];
+    
+    public CompanyProject2() {
+        setupProject("New Project");
+    }
+    
+    public CompanyProject2(String pTitle) {
+    	setupProject(pTitle);
+    }
+    
+    private void setupProject(String title) {
+    	CompanyEmailSystem.GlobalProjectCounter++;
+    	PID = CompanyEmailSystem.GlobalProjectCounter;
+        PTitle = title;
+        ProjectContacts = new ArrayList<String>();
+        ProjectPhase = 1;
+        ProjectEmails[ProjectPhase] = new ArrayList<CompanyEmail>();
+    }
+    
+    public int getPID() {
+        return PID;
+    }
+    
+    public String getPTitle() {
+        return PTitle;
+    }
+    
+	public void setPTitle(String pTitle) {
+    	if (pTitle.length() > 10 ) {
+    		PTitle = pTitle;
+    	}
+    }
+    
+    public boolean isContact(String emailAddress) {
+        return ProjectContacts.contains(emailAddress);
+    }
+    
+    public void addContact(String emailAddress) {
+        ProjectContacts.add(emailAddress);
+    }
+    
+    public void addEmail(CompanyEmail newEmail) throws Exception {
+        if (newEmail.isValid()) {
+            ProjectEmails[ProjectPhase].add(newEmail);
+            if (ProjectContacts.contains(newEmail.fromAddress())) {
+                //do nothing
+            } else {
+                ProjectContacts.add(newEmail.fromAddress());
+            }
+        } else {
+        	throw new Exception();
+        }
+    }
+    
+    public ArrayList<CompanyEmail> getEmailsForPhase() {
+        return ProjectEmails[ProjectPhase];
+    }
+    
+    public ArrayList<CompanyEmail> getEmailsForPhase(int thePhase) {
+        return ProjectEmails[thePhase];
+    }
+    
+    public boolean nextPhase() {
+        ProjectPhase++;
+        if (ProjectPhase > CompanyEmailSystem.ProjectPhases.length) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    
+    public String getPhaseByName() {
+        return CompanyEmailSystem.ProjectPhases[ProjectPhase];
+    }
+    
+    public int getPhaseByID() {
+        return ProjectPhase;
+    }
+    
+    public ArrayList<String> getProjectContacts() {
+        return ProjectContacts;
+    }
+    
+    public String toString() {
+        return PTitle + " [" + getPhaseByName() + "]";
+    }
+}
