19f211ab452389eee1565f747d6c0b535049a1bd
Henry Hunt <henryhunt.dev@gmail.com>
2018-04-30 10:52:03 +0100

    Formatting unification and fixed def. const. test

--- FSE Coursework 4/src/tests/TestCompanyEmail.java
+++ FSE Coursework 4/src/tests/TestCompanyEmail.java
@@ -11,7 +11,7 @@ import emailScripts.CompanyEmail;
 
 public class TestCompanyEmail {
 	
-	// -------Data----------
+	// ------- Data ----------
 	String fAddress ="sender@nottingham.ac.uk";
 	String tAddress ="receiver@nottingham.ac.uk";
 	String subLine = "subject";
@@ -20,79 +20,74 @@ public class TestCompanyEmail {
 	String newAddrInvalid = "psyjk4@nott";
 	
 	CompanyEmail data = new CompanyEmail();
-	// ---------------------
+	// ----------------------
 	
 	
 	//	Test default constructor
 	
 	//	Test ID: A.1.1
 	// 	Test created by: Henry Hunt
-	@Before
-	public void beforeDefaultConstructor() {
-		data = new CompanyEmail();
-	}
-	
+	//  Test created: 26/04/18
 	@Test
 	public void testDefaultConstructor() {
+		data = new CompanyEmail();
+		
 		assertNull(data.fromAddress());
 		assertNull(data.toAddress());
 		assertNull(data.subjectLine());
 		assertNull(data.emailMessage());
 	}
 		
+	
 	//	Test main constructor
 		
 	//	Test ID: A.2.1
 	// 	Test created by: Julian Kubelec
 	//	Date created: 26/04/18 
 	@Test
-		public void testMainConstructor_1() {
-			assertNotNull(new CompanyEmail(fAddress, tAddress, subLine, eMessage));
+	public void testMainConstructor_1() {
+		assertNotNull(new CompanyEmail(fAddress, tAddress, subLine, eMessage));
 	}
 
 
-
-	//Test get method for sender’s address
+	//  Test get method for sender’s address
+	
 	//	Test ID: A.3.1
 	// 	Test created by: Henry Hunt
-	//	Date created: 27/04/2018
+	//	Date created: 27/04/18
 	@Test
 	public void testFromAddress_1() {
-		data = new CompanyEmail(
-				fAddress, null, null, null);
+		data = new CompanyEmail(fAddress, null, null, null);
 		assertEquals(data.fromAddress(), fAddress);
 	}
 	
 	//	Test ID: A.3.2
 	// 	Test created by: Henry Hunt
-	//	Date created: 27/04/2018
+	//	Date created: 27/04/18
 	@Test
 	public void testFromAddress_2() {
-		data = new CompanyEmail(
-				null, null, null, null);
+		data = new CompanyEmail(null, null, null, null);
 		assertNull(data.fromAddress());
 	}
 	
 	
+	//	Test get method for receiver’s address 
 	
-//	Test get method for receiver’s address 
-
-//	Test ID: A.4.1
-// 	Test created by: Julian Kubelec
-//	Date created: 26/04/18
+	//	Test ID: A.4.1
+	// 	Test created by: Julian Kubelec
+	//	Date created: 26/04/18
 	@Test
 	public void testToAddress_1() {
-		data = new CompanyEmail(
-				null, "receiver@nottingham.ac.uk", null, null);
+		data = new CompanyEmail(null, "receiver@nottingham.ac.uk", null, null);
 		assertEquals(data.toAddress(), "receiver@nottingham.ac.uk");
 	}
-//	Test ID: A.4.2
-// 	Test created by: Julian Kubelec
-//	Date created: 26/04/18
+	
+	//	Test ID: A.4.2
+	// 	Test created by: Julian Kubelec
+	//	Date created: 26/04/18
 	@Test
 	public void testToAddress_2() {
-		data = new CompanyEmail(
-				null, null, null, null);
+		data = new CompanyEmail(null, null, null, null);
 		assertNull(data.toAddress());
 	}
 	
@@ -101,21 +96,19 @@ public class TestCompanyEmail {
 	
 	//	Test ID: A.5.1
 	// 	Test created by: Henry Hunt
-	//	Date created: 27/04/2018
+	//	Date created: 27/04/18
 	@Test
 	public void testSubjectLine_1() {
-		data = new CompanyEmail(
-				null, null, "subject", null);
+		data = new CompanyEmail(null, null, "subject", null);
 		assertEquals(data.subjectLine(), "subject");
 	}
 		
 	//	Test ID: A.5.2
 	// 	Test created by: Henry Hunt
-	//	Date created: 27/04/201
+	//	Date created: 27/04/18
 	@Test
 	public void testSubjectLine_2() {
-		data = new CompanyEmail(
-				null, null, null, null);
+		data = new CompanyEmail(null, null, null, null);
 		assertNull(data.subjectLine());
 	}
 	
@@ -124,29 +117,28 @@ public class TestCompanyEmail {
 	
 	//	Test ID: A.6.1
 	// 	Test created by: Henry Hunt
-	//	Date created: 27/04/2018
+	//	Date created: 27/04/18
 	@Test
 	public void testEmailMessage_1() {
-		data = new CompanyEmail(
-				null, null, null, eMessage);
+		data = new CompanyEmail(null, null, null, eMessage);
 		assertEquals(data.emailMessage(), eMessage);
 	}
 		
 	//	Test ID: A.6.2
 	// 	Test created by: Henry Hunt
-	//	Date created: 27/04/2018
+	//	Date created: 27/04/18
 	@Test
 	public void testEmailMessage_2() {
-		data = new CompanyEmail(
-				null, null, null, null);
+		data = new CompanyEmail(null, null, null, null);
 		assertNull(data.emailMessage());
 	}
 	
-//	Test set method for sender’s address
-
-//	Test ID: A.7.1
-// 	Test created by: Julian Kubelec
-//	Date created: 26/04/18
+	
+	//	Test set method for sender’s address
+	
+	//	Test ID: A.7.1
+	// 	Test created by: Julian Kubelec
+	//	Date created: 26/04/18
 	@Test
 	public void testSetFrom_1() {
 		data = new CompanyEmail();
@@ -154,9 +146,9 @@ public class TestCompanyEmail {
 		assertEquals(data.fromAddress(), newAddrValid);
 	}
 	
-//	Test ID: A.7.2
-// 	Test created by: Julian Kubelec
-//	Date created: 26/04/18
+	//	Test ID: A.7.2
+	// 	Test created by: Julian Kubelec
+	//	Date created: 26/04/18
 	@Test
 	public void testSetFrom_2() {
 		data = new CompanyEmail();
@@ -164,66 +156,63 @@ public class TestCompanyEmail {
 		assertNotEquals(data.fromAddress(), newAddrInvalid);
 	}
 
-//	Test ID: A.7.3
-// 	Test created by: f_name s_name
-//	Date created: dd/mm/yy
+	//	Test ID: A.7.3
+	// 	Test created by: f_name s_name
+	//	Date created: dd/mm/yy
 	@Test
 	public void testSetFrom_3() {
 		
 	}
 	
-//	Test ID: A.7.4
-// 	Test created by: f_name s_name
-//	Date created: dd/mm/yy
+	//	Test ID: A.7.4
+	// 	Test created by: f_name s_name
+	//	Date created: dd/mm/yy
 	@Test
 	public void testSetFrom_4() {
 		
 	}
 	
-//	Test set method for receiver’s address
-
-//	Test ID: L.N.N
-// 	Test created by: f_name s_name
-//	Date created: dd/mm/yy
 	
-//	Test ID: L.N.N
-// 	Test created by: f_name s_name
-//	Date created: dd/mm/yy
+	//	Test set method for receiver’s address
 	
+	//	Test ID: L.N.N
+	// 	Test created by: f_name s_name
+	//	Date created: dd/mm/yy
+		
+	//	Test ID: L.N.N
+	// 	Test created by: f_name s_name
+	//	Date created: dd/mm/yy
+		
+		
+	//	Test set method for subject line
 	
+	//	Test ID: L.N.N
+	// 	Test created by: f_name s_name
+	//	Date created: dd/mm/yy
+		
+	//	Test ID: L.N.N
+	// 	Test created by: f_name s_name
+	//	Date created: dd/mm/yy
+		
+		
+	//	Test set method for email body
 	
-//	Test set method for subject line
-
-//	Test ID: L.N.N
-// 	Test created by: f_name s_name
-//	Date created: dd/mm/yy
+	//	Test ID: L.N.N
+	// 	Test created by: f_name s_name
+	//	Date created: dd/mm/yy
+		
+	//	Test ID: L.N.N
+	// 	Test created by: f_name s_name
+	//	Date created: dd/mm/yy
+		
+		
+	//	Test function to check validity
 	
-//	Test ID: L.N.N
-// 	Test created by: f_name s_name
-//	Date created: dd/mm/yy
-	
-	
-	
-//	Test set method for email body
-
-//	Test ID: L.N.N
-// 	Test created by: f_name s_name
-//	Date created: dd/mm/yy
-	
-//	Test ID: L.N.N
-// 	Test created by: f_name s_name
-//	Date created: dd/mm/yy
-	
-	
-	
-//	Test function to check validity
-
-//	Test ID: L.N.N
-// 	Test created by: f_name s_name
-//	Date created: dd/mm/yy
-	
-//	Test ID: L.N.N
-// 	Test created by: f_name s_name
-//	Date created: dd/mm/yy
-
+	//	Test ID: L.N.N
+	// 	Test created by: f_name s_name
+	//	Date created: dd/mm/yy
+		
+	//	Test ID: L.N.N
+	// 	Test created by: f_name s_name
+	//	Date created: dd/mm/yy
 }
