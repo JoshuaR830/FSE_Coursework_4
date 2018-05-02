34e60fd641dd5d0bb86357ab45d08643044e72c2
Joshua Richardson <psyjr4@nottingham.ac.uk>
2018-05-02 13:13:04 +0100

    Changed C.8.3

--- FSE Coursework 4/src/tests/TestCompanyEmailSystem.java
+++ FSE Coursework 4/src/tests/TestCompanyEmailSystem.java
@@ -186,6 +186,8 @@ public class TestCompanyEmailSystem {
 //	Date created: 02/05/18
 	@Test
 	public void testChangeProjectPhase_C83() {
+		CompanyEmailSystem ces = new CompanyEmailSystem();
+		ces.ChangeProjectPhase();
 		
 	}
 	
